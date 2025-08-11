67cad2f52d889dc403186e55d307c30e6c0b836c
Vasilije Bozaric <89221348@student.upr.si>
2025-08-11 21:37:08 +0200

    prepare for deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
-const BACKEND_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
+const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
